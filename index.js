b8ad9ff2da81c07fdba17a9866383e2e14b43c37
KMkansi <KMkansi@BSS-L-VMAKOLE>
2022-05-23 11:51:55 +0200

    files

---
+++ index.js
@@ -0,0 +1,60 @@
+const form = document.querySelector("form"),
+statusTxt = form.querySelector(".button-area span");
+
+form.onsubmit = (e)=>{
+  e.preventDefault();
+  statusTxt.style.color = "#0D6EFD";
+  statusTxt.style.display = "block";
+  statusTxt.innerText = "Sending your message...";
+  form.classList.add("disabled");
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "contact-form-process.php", true);
+  xhr.onload = ()=>{
+    if(xhr.readyState == 4 && xhr.status == 200){
+      let response = xhr.response;
+      if(response.indexOf("Email and message field is required!") != -1 || response.indexOf("Enter a valid email address!") != -1 || response.indexOf("Sorry, failed to send your message!") != -1){
+        statusTxt.style.color = "red";
+      }else{
+        form.reset();
+        setTimeout(()=>{
+          statusTxt.style.display = "none";
+        }, 3000);
+      }
+      statusTxt.innerText = response;
+      form.classList.remove("disabled");
+    }
+  }
+  let formData = new FormData(form);
+  xhr.send(formData);
+}
+
+
+function readMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+    var less = document.getElementById("less")
+
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      less.style.display = "none";
+      //btnText.textContent = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      less.style.display = "inline";
+      //btnText.textContent  = 'Read less'; 
+      moreText.style.display = "inline";
+    }
+  }
+
+  function readLess() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+    moreText.style.display = "none";
+    less.style.display = "none";
+    
+  }
